247542331f600964faa246d5fd5f236807aebc0b
DylanC <comeaudc@comcast.net>
2026-02-17 12:01:05 -0500

    Connected to DB

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // Import
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db/conn.js";
 
 // Setups
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
+connectDB();
 
 // MIddleware
 
