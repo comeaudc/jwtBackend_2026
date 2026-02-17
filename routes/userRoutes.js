d653aded875169b0b0076cedaa856b4aee5c0983
DylanC <comeaudc@comcast.net>
2026-02-17 13:45:06 -0500

    Finished register route; no testing

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 import User from "../models/userSchema.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
 import { check, validationResult } from "express-validator";
+dotenv.config();
 
 const router = express.Router();
 
@@ -29,7 +31,59 @@ router
       if (!errors.isEmpty())
         return res.status(400).json({ errors: errors.array() });
 
-      res.send("testing user");
+      const { name, email, password } = req.body;
+
+      try {
+        //  Check if user already exists
+        let user = await User.findOne({ email });
+
+        // Respond with error
+        if (user)
+          return res
+            .status(400)
+            .json({ errors: [{ msg: "User already exists" }] });
+
+        //  Create new user object
+        user = new User({
+          name,
+          email,
+          password,
+        });
+
+        // Create salt to hash our pw. Number represents number of rounds of encryption
+        const salt = await bcrypt.genSalt(10);
+
+        // Hash/Encrpyt our pw
+        user.password = await bcrypt.hash(password, salt);
+
+        // Save user to DB
+        await user.save();
+
+        // Create payload for JWT
+        const payload = {
+          user: {
+            id: user._id,
+            name: user.name, // normally would not include name
+          },
+        };
+
+        //Respond with JWT, because you register and login simulatneously
+        jwt.sign(
+          payload,
+          process.env.jwtSecret,
+          { expiresIn: "3h" },
+          (err, token) => {
+            if (err) throw err;
+
+            res.json({ token });
+          },
+        );
+      } catch (error) {
+        console.error(error.message);
+        res
+          .status(error.status || 500)
+          .json({ errors: [{ msg: `❌ Error: ${error.message}` }] });
+      }
     },
   );
 
