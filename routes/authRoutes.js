dcac455031292dbc32b38fbbf732a8991d1fb721
DylanC <comeaudc@comcast.net>
2026-02-17 13:58:15 -0500

    Added imports

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
 import User from "../models/userSchema.js";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { check, validationResult } from "express-validator";
+dotenv.config();
 
 const router = express.Router();
 
-router.route('/').get((req,res) => {
-    res.send('testing auth')
-})
+//  @route: POST /api/auth
+//  @desc: Login User
+//  @access: Public
+router.route("/").post((req, res) => {
+  res.send("testing auth");
+});
 
 export default router;
